2811b04126111a73bb6ae81a7f53e6797ee8e79b
Gustavo <gustavoabsampaio@gmail.com>
2020-01-15 18:34:56 -0300

    Department, Employees and Roles admin access finished

--- app/admin/views.py
+++ app/admin/views.py
@@ -2,9 +2,9 @@ from flask import abort,flash,redirect,render_template,url_for
 from flask_login import current_user, login_required
 
 from . import admin
-from .forms import DepartmentForm, RoleForm
+from .forms import DepartmentForm, EmployeeAssignForm, RoleForm
 from .. import db
-from ..models import Department, Role
+from ..models import Department, Employee, Role
 
 def check_admin():
     if not current_user.is_admin:
@@ -143,3 +143,34 @@ def delete_role(id):
 
     return render_template(title="Delete Role")
 
+@admin.route('/employees')
+@login_required
+def list_employees():
+
+    check_admin()
+
+    employees = Employee.query.all()
+    return render_template('admin/employees/employees.html',employees=employees,title='Employees')
+
+@admin.route('/employees/assign/<int:id>',methods=['GET','POST'])
+@login_required
+def assign_employee(id):
+
+    check_admin()
+
+    employee = Employee.query.get_or_404(id)
+
+    if employee.is_admin:
+        abort(403)
+
+    form = EmployeeAssignForm(obj=employee)
+    if form.validate_on_submit():
+        employee.department = form.department.data
+        employee.role = form.role.data
+        db.session.add(employee)
+        db.session.commit()
+        flash('Department and role successfully assigned')
+
+        return redirect(url_for('admin.list_employees'))
+
+    return render_template('admin/employees/employee.html',employee=employee,form=form,title='Assign Employee')
